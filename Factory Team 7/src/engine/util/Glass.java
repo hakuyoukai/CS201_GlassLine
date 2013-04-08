706fdf7a7b7bd9f6fee3ca71ec4d4b4fee64620d
lang1991 <tlang@usc.edu>
2013-04-07 18:29:28 -0700

    online code added

--- Factory Team 7/src/engine/util/Glass.java
+++ Factory Team 7/src/engine/util/Glass.java
@@ -6,18 +6,10 @@ import java.util.Map;
 
 public class Glass
 {
-	public Map<Integer,Integer> recipe;
+	public Map<Integer,Boolean> recipe;
 	
 	public Glass(String barcode)
 	{
 		recipe = new HashMap();
-		if(barcode.equals("0"))
-		{
-			recipe.put(2, 0);
-		}
-		else if(barcode.equals("1"))
-		{
-			recipe.put(2, 1);
-		}
 	}
 }
