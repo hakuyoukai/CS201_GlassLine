c060aeef456ecc92fced07753d88a40035a6c6cc
Michael Whitaker <mcwhitak@usc.edu>
2013-04-12 16:28:20 -0700

    Just changes made to my legacy Conveyor Family

---
+++ Factory Team 7/src/engine/conveyorfamily/last/ConveyorFamilyLast.java
@@ -0,0 +1,21 @@
+package engine.conveyorfamily.last;
+
+import engine.util.ConveyorFamilyInterface;
+import engine.util.Glass;
+
+public class ConveyorFamilyLast implements ConveyorFamilyInterface
+{
+	private Integer number;
+
+	@Override
+	public void msgHereIsGlass(Glass g)
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void msgIAmReady()
+	{
+		// TODO Auto-generated method stub
+	}
+}
