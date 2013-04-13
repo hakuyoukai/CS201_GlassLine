508084db27265fd34173e2fdca7242bdb585ea04
Dexter Tan <dexter.c.tan@gmail.com>
2013-04-13 13:46:38 -0700

    adding my conveyor family

---
+++ Factory Team 7/src/engine/conveyorfamily/d/Popup.java
@@ -0,0 +1,10 @@
+package engine.conveyorfamily.d;
+
+//import generalClasses.Glass;
+import engine.util.*;
+
+public interface Popup {
+	public void msgHereIsGlass(Glass g);
+	public void msgNextConveyorReady();
+	public void msgNextConveyorNotReady();
+}
